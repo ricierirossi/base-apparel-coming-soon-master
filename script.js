3234bce890f913c14e665321ee43205b7032d81e
ricirossi <matheus.ricieri@hotmail.com>
2023-04-30 16:58:26 -0300

    Start changing HTML and CSS
    Change HTML so default code become mobile then use CSS media queries to make it desktop size. Revise IDs and Classes names.

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function checkEmail () {
     const value = document.querySelector('input').value
     if (status || value == "") {
       const warning = document.querySelector('#warning')
-      document.getElementById('warning').classList.remove('warning-none')
+      document.getElementById('warning').classList.remove('warning-hidden')
       document.getElementById('warning').classList.add('warning-visible')
+      // document.getElementById('field').style.margin = "0px 0px 50px 100px"
       document.getElementById('field').style.border = "solid 2px var(--soft-red)"
+      document.querySelector('.').style.padding = '0px'
     } else {
       document.getElementById('warning').classList.remove('warning-visible')
-      document.getElementById('warning').classList.add('warning-none')
+      document.getElementById('warning').classList.add('warning-hidden')
       document.getElementById('field').style.border = "solid 1px hsl(0, 80%, 86%)"
       // Recebe o modal
       var modal = document.querySelector('.modal-container')
@@ -20,7 +22,7 @@ function checkEmail () {
       // Abrir modal
       modal.style.display = "flex";
       // Limpar email
-      document.querySelector('.data-field').value = "";
+      document.querySelector('.email-field').value = "";
       // Borrar página
       document.querySelector('.container').classList.add('blur')
       // Fechar modal
